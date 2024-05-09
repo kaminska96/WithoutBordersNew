80a554a084b2592fba3a2b013e41ee09653d39c3
kaminska96 <yuliia.kaminska@gmail.com>
2024-05-09 22:55:13 +0300

    update

--- wbproject/wbproject/settings.py
+++ wbproject/wbproject/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'wbproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'wb',
+        'NAME': 'wbnew2',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
     }
